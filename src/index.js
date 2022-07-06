4a22716257fc95e3841cbc5879c3ce47ef63b8ac
healingfields <dihratene@gmail.com>
2022-07-07 00:29:38 +0100

    updated index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import ReactDom from 'react-dom/client';
-
+import List from './components/List';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-    return <h1>rick and morty</h1>;
-}
+    return (
+        <div className='container'>
+            <nav className='navbar sticky-top navbar-light bg-dark mb-4'>
+                <h1 className='text-center text-light fs-3 fw-bold '>Rick and morty</h1>
+            </nav>
+            <List />
+        </div>
+    )
+};
 
 const container = document.getElementById("app");
 const root = ReactDom.createRoot(container);
-root.render(<App />);
+root.render(<App />); 
